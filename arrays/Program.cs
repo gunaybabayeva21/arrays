94ee144a32cb7f58e2b70b0b2e2226c06926e8de
gunaybabayeva21 <gunaybabayeva032@gmail.com>
2023-03-01 07:49:26 +0400

    new project

---
+++ arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿//Task 1 (arrays)
+
+//int [] array = {2, 3 , 6, 9 ,8};
+//int sum = 0;
+//for (int i = 0; i < array.Length; i++)
+//{
+//     sum += array[i] ;
+
+//}
+//Console.WriteLine(sum);
+
+
+
+//Task 2 , uzunlugu 4 den boyuk olanlari ekrana cixar.
+
+
+//int x = 4;
+//string[] strings = { "gunay", "ayde", "shabaz", "lale" };
+//for (int i = 0; i< strings.Length; i++)
+//{
+//    if (strings[i].Length > x) 
+//    {
+//        Console.WriteLine(strings[i]);
+//    }
+   
+//}
+
+
+
